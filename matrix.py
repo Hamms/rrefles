6a08c66764f481e7be6171854a7193bad9c1c059
Hamms <hamms@thebeast.(none)>
2011-04-28 16:06:35 -0700

    added transposition, changed toString options to default to true

--- matrix.py
+++ matrix.py
@@ -9,7 +9,10 @@ class Matrix:
       list = []
       for y in x:
         if isinstance(y,str):
-          y = int(y)
+          try:
+            y = int(y)
+          except:
+            y = Variable(y)
         if isinstance(y,int):
           y = Fraction(y)
         list.append(y)
@@ -21,6 +24,15 @@ class Matrix:
         if y>=start_row and not self.rows[y][x] == 0 and not isinstance(self.rows[y][x],Variable):
           return (y,x)
     return (None,None)
+
+  def transpose(self):
+    that = []
+    for j in range(len(self[0])):
+      row = []
+      for i in range(len(self)):
+        row.append(self[i][j])
+      that.append(row)
+    return Matrix(that)
 
   def swap(self,one,two):
     if not one == two:
@@ -69,7 +81,6 @@ class Matrix:
               val += self[i][x] * other[x][j]
             row.append(val)
           this.append(row)
-        print this
         return Matrix(this)
       else:
         TypeError("cannot multiply a " + str(len(self)) + "x" + str(len(self[0])) + "matrix by a " + str(len(other)) + str(len(other[0])) + "matrix")
@@ -94,13 +105,13 @@ class Matrix:
   def toString(self,options={}):
     for opt in ["nobar","compact"]:
       if opt not in options:
-        options[opt] = False
+        options[opt] = True
     ret = "\left[ \\begin{array}{"
     for _ in range(len(self.rows[0])-1):
-      ret += "c "
+      ret += "r "
     if not options['nobar']:
       ret += "| "
-    ret += "c}"
+    ret += "r}"
     if not options['compact']:
       ret += "\n"
     for r in self.rows:
@@ -111,6 +122,6 @@ class Matrix:
     return ret
 
   def __str__(self):
-    return self.toString({})
+    return self.toString({'nobar':False,'compact':False})
     
 
